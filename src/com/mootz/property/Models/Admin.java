98164177c2b16c1ab1cfe53eca77dc1635445c6c
Seb Heron <sebheron21@hotmail.co.uk>
2020-08-05 18:01:57 +0100

    Added admin comments

--- src/com/mootz/property/Models/Admin.java
+++ src/com/mootz/property/Models/Admin.java
@@ -5,32 +5,39 @@ public class Admin {
 	private String username;
 	private String password;
 	
+	//Creates a new admin.
 	public Admin(String name, String username, String password) {
 		this.name = name;
 		this.username = username;
 		this.password = password;
 	}
 	
+	//Sets the name of the admin.
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	//Sets the username for the admin.
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	
+	//Sets the password for the admin.
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	//Gets the name of the admin.
 	public String getName() {
 		return this.name;
 	}
 	
+	//Gets the admin username.
 	public String getUsername() {
 		return this.username;
 	}
 	
+	//Gets the admin password.
 	public String getPassword() {
 		return this.password;
 	}
