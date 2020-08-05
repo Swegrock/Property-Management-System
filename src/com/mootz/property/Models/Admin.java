0001a3ebd690de41006e76cb212d070fa4b8980a
Seb Heron <sebheron21@hotmail.co.uk>
2020-08-05 14:22:14 +0100

    Filled out admin class

--- src/com/mootz/property/Models/Admin.java
+++ src/com/mootz/property/Models/Admin.java
@@ -1,5 +1,37 @@
 package com.mootz.property.Models;
 
 public class Admin {
-
+	private String name;
+	private String username;
+	private String password;
+	
+	public Admin(String name, String username, String password) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
 }
